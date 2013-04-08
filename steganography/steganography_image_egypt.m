f0f09b60d943966c024ecb02f2523faa3dad30a8
Sam Hooke <marbsam@yahoo.co.uk>
2013-04-08 17:16:40 +0100

    Created steganography_image_egypt.m

---
+++ steganography/steganography_image_egypt.m
@@ -0,0 +1,23 @@
+clc;
+clear variables;
+[dir_input, dir_output] = steganography_init();
+
+% Encode
+% ======
+
+%@@ Input image and output location
+carrier_image_filename = [dir_input, 'lena.jpg'];
+output_image_filename = [dir_output, 'lena_egypt.jpg'];
+
+%@@ Message string to encode into carrier image
+%@@ Leave blank to automatically generate a message
+secret_msg_str = '';
+
+%@@ Which colour channel to use (1=r, 2=g, 3=b)
+channel = 3;
+
+%@@ Output image quality
+output_quality = 100;
+
+%@@ Wavelet transformation
+mode = 'db1';
