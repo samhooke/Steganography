8c18fde5e9177bcf7221ff082a54e6382d1fb117
Sam Hooke <marbsam@yahoo.co.uk>
2013-04-08 13:17:51 +0100

    Tidied up bin and str conversion functions

--- steganography/helper/bin2binstr.m
+++ steganography/helper/bin2binstr.m
@@ -1,6 +1,10 @@
-function s = bin2binstr( a )
+function s = bin2binstr(a)
+% bin2str() Converts binary into a string of 1s and 0s
+% INPUTS
+%    a - Input binary
+% OUTPUTS
+%    s - Output string of 1s and 0s
 
-% Converts binary into a character array of 1s and 0s
 s = char(a + 48);
 
 end
--- steganography/helper/str2bin.m
+++ steganography/helper/str2bin.m
@@ -1,5 +1,12 @@
-function a = str2bin( s )
+function a = str2bin(s)
+% str2bin() Converts a string into binary
+% INPUTS
+%    s - Input string
+% OUTPUTS
+%    a - Output binary
+
 a = dec2bin(s, 8)';
 a = a(:)'-'0';
+
 end
 
