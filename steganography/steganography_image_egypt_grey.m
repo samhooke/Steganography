3262bcc7da85e62672d067779b7d2c7909fdc7ef
Sam Hooke <marbsam@yahoo.co.uk>
2013-04-08 17:43:05 +0100

    Will implement Egypt method in greyscale first

--- steganography/steganography_image_egypt_grey.m
+++ steganography/steganography_image_egypt_grey.m
@@ -7,14 +7,8 @@ clear variables;
 
 %@@ Input image and output location
 carrier_image_filename = [dir_input, 'lena.jpg'];
+secret_image_filename = [dir_input, 'peppers.jpg'];
 output_image_filename = [dir_output, 'lena_egypt.jpg'];
-
-%@@ Message string to encode into carrier image
-%@@ Leave blank to automatically generate a message
-secret_msg_str = '';
-
-%@@ Which colour channel to use (1=r, 2=g, 3=b)
-channel = 3;
 
 %@@ Output image quality
 output_quality = 100;
