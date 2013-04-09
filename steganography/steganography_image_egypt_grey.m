d49c47711cda3aa77b58e95d6a14a4be16ea2385
Sam Hooke <marbsam@yahoo.co.uk>
2013-04-09 17:43:56 +0100

    Tidied up Egypt algorithm

--- steganography/steganography_image_egypt_grey.m
+++ steganography/steganography_image_egypt_grey.m
@@ -1,37 +1,8 @@
-clc;
-clear variables;
-[dir_input, dir_output] = steganography_init();
-
-tic
-
-% Encode
-% ======
-
-%@@ Input image and output location
-carrier_image_filename = [dir_input, 'lena.jpg'];
-secret_image_filename = [dir_input, 'peppers.jpg'];
-output_image_filename = [dir_output, 'lena_egypt.jpg'];
-
-%@@ Output image quality
-output_quality = 100;
-
-%@@ Wavelet transformation
-mode = 'db1';
-%db# 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45
-%coif# 3
-%sym# 1, 5...
-%bior#.# 1.1, 1.5, 2.4, 2.8, 4.4, 6.8
-%rbio#.# 1.1, 1.5...
-
-
-%@@ Whether each value in the key must be unique
-unique_k1 = false;
-unique_k2 = false;
-
-%@@ Block size
-block_size = 4;
-
 %{
+
+Encode
+======
+
 C = im_carrier
 S = im_secret
 G = im_stego
@@ -62,7 +33,57 @@ G = im_stego
 (6) Repeat (3) to (5) until all error blocks are embedded in CHL1.
 
 (7) Apply 2D-IDWT to (CLL1, CLH1, CHH1, CHL1) to obtain G.
+
+
+Decode
+======
+
+(1) Perform 1 level 2D-IDWT on G to obtain 4 subimages:
+    (GLL1, GLH1, GHL1, GHH1)
+(2) Extract block BCk1 from GLL1 using K1
+    Extract block EBi from GHL1 using K2
+    Obtain BSi with:
+        BSi = BCk1 - EBi
+(3) Repeat (2) until all secret blocks are extracted and form subimage SLL1
+(4) Set SHL1, SLH1 and SHH1 to zeros. Apply 2D-IDWT to obtain secret image
+
 %}
+
+clc;
+clear variables;
+[dir_input, dir_output] = steganography_init();
+
+% Encode
+% ======
+
+%@@ Input image and output location
+carrier_image_filename = [dir_input, 'lena_tiny.jpg'];
+secret_image_filename = [dir_input, 'peppers_tiny.jpg'];
+output_image_filename = [dir_output, 'lena_egypt.jpg'];
+
+%@@ Output image quality
+output_quality = 75;
+
+%@@ Wavelet transformation
+%db# 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45
+%coif# 3
+%sym# 1, 5...
+%bior#.# 1.1, 1.5, 2.4, 2.8, 4.4, 6.8
+%rbio#.# 1.1, 1.5...
+mode = 'db1';
+
+%@@ Whether each value in the key must be unique
+%@@ [Default: false, false]
+unique_k1 = false;
+unique_k2 = false;
+
+%@@ Block size
+%@@ [Default: 4]
+block_size = 4;
+
+%@@ Whether to overwrite BH during encoding, or use a temporary copy
+%@@ [Default: false]
+overwrite_BH = false;
 
 % Load images as greyscale, for simplicity in this first implementation
 im_carrier = rgb2gray(imread(carrier_image_filename));
@@ -118,11 +139,10 @@ for i = 1:ns
     EB{i} = BC{k1} - BS{i};
 end
 
-% Calculate error block (EBi = BCk1 - BSi)
-%EB = cellfun(@minus, BC, BS, 'Un', 0);
-
 % Make a copy of CHL1, which will be modified
-BH_stego = BH;
+if ~overwrite_BH
+    BH_stego = BH;
+end
 
 for i = 1:ns
     % For each EBi, find the best matched block BHk2 in CHL1
@@ -147,11 +167,19 @@ for i = 1:ns
     key2(i) = best_k2;
     
     % Also, replace the block
-    BH_stego{best_k2} = EB{i};
+    if overwrite_BH
+        BH{best_k2} = EB{i};
+    else
+        BH_stego{best_k2} = EB{i};
+    end
 end
 
 % Form CHL1 with the stego stuff
-CHL1_stego = cell2mat(reshape(BH_stego, cw, ch)');
+if overwrite_BH
+    CHL1_stego = cell2mat(reshape(BH, cw, ch)');
+else
+    CHL1_stego = cell2mat(reshape(BH_stego, cw, ch)');
+end
 
 im_wavelet = [CLL1, CLH1; CHL1_stego, CHH1];
 im_stego = uint8(idwt2(CLL1, CLH1, CHL1_stego, CHH1, mode));
@@ -162,17 +190,6 @@ imwrite(im_stego, output_image_filename, 'Quality', output_quality);
 % ======
 
 % Need key1, key2 & im_stego (K1, K2 & G)
-
-%{
-(1) Perform 1 level 2D-IDWT on G to obtain 4 subimages:
-    (GLL1, GLH1, GHL1, GHH1)
-(2) Extract block BCk1 from GLL1 using K1
-    Extract block EBi from GHL1 using K2
-    Obtain BSi with:
-        BSi = BCk1 - EBi
-(3) Repeat (2) until all secret blocks are extracted and form subimage SLL1
-(4) Set SHL1, SLH1 and SHH1 to zeros. Apply 2D-IDWT to obtain secret image
-%}
 
 % Load G
 im_stego = imread(output_image_filename);
@@ -195,8 +212,6 @@ SLH1 = zeros(cw * block_size, ch * block_size);
 SHH1 = zeros(cw * block_size, ch * block_size);
 
 im_extracted = uint8(idwt2(SLL1, SLH1, SHL1, SHH1, mode));
-
-toc
 
 subplot(2,2,1);
 imshow(im_wavelet, [0 255]);
